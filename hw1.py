0494d0d739a3f07c1cabd084dfc82b6e251a3baa
kwickin123 <90592700+kwickin123@users.noreply.github.com>
2021-10-14 22:47:19 +0530

    Add files via upload

---
+++ hw1.py
@@ -0,0 +1,15 @@
+print("""1)addition
+2)subtraction
+3)multiplication
+4)division""")
+a=int(input("What operation:"))
+b=int(input("first number:"))
+c=int(input("second number:"))
+if a==1:
+    print("The Answer is:",a+b)
+if a==2:
+    print("The Answer is:",a-b)
+if a==3:
+    print("The Answer is:",a*b)
+if a==4:
+    print("The Answer is:",a/b)
---
+++ hw2.py
@@ -0,0 +1,5 @@
+#slicing string program
+a="hello"
+b="world"
+c=a+b
+print(c[:3])
